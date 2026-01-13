56e9ebca966521333d968cc0709f585c77b4b312
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-01-13 11:26:53 +0530

    adding this file to repo

---
+++ Arrays_Count-Occurrences-of-an-Element.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+Input Format:
+
+The first line contains an integer n, the number of elements in the array.
+The second line contains n space-separated integers representing the array elements.
+The third line contains a single integer, the target element whose occurrences you need to count.
+
+Output Format:
+
+Print a single integer representing the number of times the target element occurs in the array.
+
+
+int main() {
+
+    int N,K,count;
+    
+    scanf("%d",&N);
+    
+    int a[N];
+    
+    for(int i=0;i<=N-1;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    scanf("%d",&K);
+    
+    for(int i=0;i<=N-1;i++)
+    {
+        if(a[i]==K)
+        {
+            count++;
+        }
+    }
+    
+    printf("%d",count);
+    
+    
+    return 0;
+}
