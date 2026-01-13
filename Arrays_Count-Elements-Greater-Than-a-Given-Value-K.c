05823f83f75764919ac1bdfb7c62c4f878a616d0
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-01-13 21:11:31 +0530

    adding this file to the repo

--- Arrays_Count-Elements-Greater-Than-a-Given-Value-K.c
+++ Arrays_Count-Elements-Greater-Than-a-Given-Value-K.c
@@ -1,42 +1,59 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
+// Input Format:
 
-Input Format:
+// First line: An integer N, representing the size of the array
+// Second line: N space-separated integers
+// Third line: An integer K
 
-First line: An integer N, representing the size of the array
-Second line: N space-separated integers
-Third line: An integer K
+// Output Format:
 
-Output Format:
+// Print a single integer representing the count of elements strictly greater than K.
 
-Print a single integer representing the count of elements strictly greater than K.
+int main()
+{
 
-int main() {
+    int N, K, count;
+    scanf("%d", &N);
 
-    int N,K,count;
-    scanf("%d",&N);
-    
     int a[N];
-    
-    for(int i=0;i<=N-1;i++)
+
+    for (int i = 0; i <= N - 1; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
-    
-    scanf("%d",&K);
-    
-    for(int i=0;i<=N-1;i++)
+
+    scanf("%d", &K);
+
+    for (int i = 0; i <= N - 1; i++)
     {
-        if(a[i]>K)
+        if (a[i] > K)
         {
             count++;
         }
     }
-    
-    printf("%d",count);
-    
+
+    printf("%d", count);
+
     return 0;
 }
+
+// Sample Input 0
+
+// 5
+// 1 3 5 7 9
+// 4
+// Sample Output 0
+
+// 3
+// Sample Input 1
+
+// 4
+// -5 -2 -9 -1
+// -3
+// Sample Output 1
+
+// 2
