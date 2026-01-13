2d99ccb0cb5e75f1688572f304ad14e4bd71986a
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-01-13 11:47:38 +0530

    adding this file to repo

---
+++ Arrays_Count-Elements-Greater-Than-a-Given-Value-K.c
@@ -0,0 +1,42 @@
+ #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+Input Format:
+
+First line: An integer N, representing the size of the array
+Second line: N space-separated integers
+Third line: An integer K
+
+Output Format:
+
+Print a single integer representing the count of elements strictly greater than K.
+
+int main() {
+
+    int N,K,count;
+    scanf("%d",&N);
+    
+    int a[N];
+    
+    for(int i=0;i<=N-1;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    scanf("%d",&K);
+    
+    for(int i=0;i<=N-1;i++)
+    {
+        if(a[i]>K)
+        {
+            count++;
+        }
+    }
+    
+    printf("%d",count);
+    
+    return 0;
+}
