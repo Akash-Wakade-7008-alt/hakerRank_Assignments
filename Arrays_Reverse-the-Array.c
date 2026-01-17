7896b91f1cb12961f2fea0da17b457433e438ff7
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-01-17 22:24:56 +0530

    adding this file to the repo

--- Arrays_Reverse-the-Array.c
+++ Arrays_Reverse-the-Array.c
@@ -1,55 +1,70 @@
 #include <stdio.h>
 
+// You are given an array of integers. Your task is to implement a function that reverses the order of the elements in the array in-place. You do not need to return anything.
 
-You are given an array of integers. Your task is to implement a function that reverses the order of the elements in the array in-place. You do not need to return anything.
+// Function Signature
 
-Function Signature
+// void reverseArray(int arr[], int n);
 
-void reverseArray(int arr[], int n);
+// Input Format:
 
-Input Format:
+// The function reverseArray will receive:
+// An integer array arr of size n.
+// An integer n representing the number of elements in the array.
 
-The function reverseArray will receive:
-An integer array arr of size n.
-An integer n representing the number of elements in the array.
+// Output Format:
 
+// The function should modify the given array in-place by reversing its elements.
+// You do not need to print anything.
 
-Output Format:
+void reverseArray(int arr[], int n)
+{
 
-The function should modify the given array in-place by reversing its elements.
-You do not need to print anything.
+    int i = 0;
+    int j = n - 1;
 
-
-void reverseArray(int arr[], int n) {
-    
-    int i=0;
-    int j=n-1;
-    
-    while(i<j)
+    while (i < j)
     {
-        int temp=arr[j];
-        arr[j]=arr[i];
-        arr[i]=temp;
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
         i++;
         j--;
     }
-    
 }
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
     int arr[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
 
     reverseArray(arr, n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
-    
+
     return 0;
 }
+
+// Sample Input 0
+
+// 6
+// 1 2 3 4 5 6
+// Sample Output 0
+
+// 6 5 4 3 2 1
+// Sample Input 1
+
+// 5
+// 5 10 15 20 25
+// Sample Output 1
+
+// 25 20 15 10 5
